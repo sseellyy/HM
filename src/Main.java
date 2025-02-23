a40d43e87616f77c655d5f24e304bdcb2278ff74
Proj <seiil.mekishova@alatoo.edu.kg>
2025-02-24 03:17:20 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        File dictionary = new File("/dictionary.txt");
+
+        Scanner textScanner = new Scanner(dictionary);
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String> words = new ArrayList<>();
+        while(textScanner.hasNextLine()){
+            words.add(textScanner.nextLine());
+
         }
+        String hidden_text = words.get((int)(Math.random()*words.size()));
+        char[] textArray = hidden_text.toCharArray();
+
+        char [] myAnswers = new char[textArray.length];
+        for(int i = 0; i < textArray.length; i++){
+            myAnswers[i] = '?';
+        }
+        boolean finished = false;
+        int lives = 6;
+
+        while(!finished){
+            System.out.println("******************");
+
+            String letter = input.next();
+            //checks for valid input
+            while(letter.length() != 1 || Character.isDigit(letter.charAt(0))){
+                System.out.println("Error input - Try again");
+                letter = input.next();
+            }
+
+            //checks if letter is in the word
+            boolean found = false;
+            for(int i = 0; i < textArray.length; i++){
+                if (letter.charAt(0) == textArray[i]){
+                    myAnswers[i] = textArray[i];
+                    found = true;
+                }
+            }
+            if(!found){
+                lives--;
+
+                System.out.println("Wrong letter");
+            }
+            boolean done = true;
+            for (char myAnswer : myAnswers) {
+                if (myAnswer == '?') {
+                    System.out.println("_");
+                    done = false;
+                } else {
+                    System.out.println(" " + myAnswer);
+                }
+            }
+            System.out.println("\n" + "Lives left: " + lives);
+
+            //checks if the game ends
+            if (done){
+                System.out.println("Congrats. You found the word");
+            }
+            if (lives <= 0){
+                System.out.println("You are dead!");
+                finished = true;
+            }
+        }
+
     }
 }
